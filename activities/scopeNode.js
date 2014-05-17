d642191143f7ff6923949c2073dbccea72efdbca
unbornchikken <gabor.mezo@outlook.com>
2014-05-17 13:40:35 +0200

    Before integrating new scope

--- activities/scopeNode.js
+++ activities/scopeNode.js
@@ -1,4 +1,3 @@
-var TreeNode = require("./treeNode");
 var util = require("util");
 var _ = require("underscore-node");
 
@@ -7,10 +6,48 @@ function ScopeNode(id, scopePart)
     TreeNode.call(this);
 
     this.id = id;
-    this._scopePart = {}
+    this._scopePart = {};
+    this._parent = null;
+    this._children = {};
 }
 
-util.inherits(ScopeNode, TreeNode);
+ScopeNode.prototype.parent = function()
+{
+    return this._parent;
+}
+
+ScopeNode.prototype.forEachToRoot = function(f)
+{
+    var current = this;
+    do
+    {
+        f.call(this, current);
+        current = current._parent;
+    }
+    while (current);
+
+}
+
+ScopeNode.prototype.addChild = function (childItem)
+{
+    if (!(childItem instanceof ScopeNode)) throw new TypeError("Node argument expected.");
+    if (childItem._parent) throw new Error("Item has been already aded to a .");
+    childItem._parent = this;
+    this._children[childItem.id] = childItem;
+}
+
+ScopeNode.prototype.removeChild = function (childItem)
+{
+    if (!(childItem instanceof ScopeNode)) throw new TypeError("Node argument expected.");
+    if (childItem._parent != this) throw new Error("Item is not a current node's child.");
+    childItem._parent = null;
+    delete this._children[childItem.id];
+}
+
+ScopeNode.prototype.addField = function(name, value)
+{
+    this._scopePart[name] = value;
+}
 
 ScopeNode.prototype.addAllFields = function(to)
 {
@@ -41,6 +78,17 @@ ScopeNode.prototype.addFieldsOf = function (to, otherNode)
         if (ov != undefined)
         {
             to[fn] = ov;
+        }
+    }
+}
+
+ScopeNode.prototype.addNonExistingAndNonPrivateFields = function(to)
+{
+    for (var fn in this._scopePart)
+    {
+        if (fn[0] !== "_" && to[fn] === undefined)
+        {
+            to[fn] = this._scopePart[fn];
         }
     }
 }
--- activities/scopeTree.js
+++ activities/scopeTree.js
@@ -10,6 +10,8 @@ function ScopeTree()
     this._currentNode = this._initialNode;
     this.currentScope = {};
     this._currentNode.addAllFields(this.currentScope);
+    this._currentFields = null;
+    this._refreshCurrentFields();
 }
 
 ScopeTree.prototype.next = function (id, scopePart)
@@ -20,6 +22,7 @@ ScopeTree.prototype.next = function (id, scopePart)
     this._currentNode.addChild(nextNode);
     this._currentNode = nextNode;
     this._nodes[id] = nextNode;
+    this._refreshCurrentFields();
 }
 
 ScopeTree.prototype.back = function(keepItem)
@@ -27,6 +30,19 @@ ScopeTree.prototype.back = function(keepItem)
     var self = this;
     
     if (self._currentNode == self._initialNode) throw new Error("Cannot go back.");
+
+    if (keepItem)
+    {
+        // We have to add current node's part all fields that new since it created:
+        for (var fn in self.currentScope)
+        {
+            if (self._currentFields[fn] === undefined)
+            {
+                var fv = self.currentScope[fn];
+                self._currentNode.addField(fn, fv);
+            }
+        }
+    }
 
     var toRemove = self._currentNode;
     toRemove.removeAllFields(self.currentScope);
@@ -46,6 +62,30 @@ ScopeTree.prototype.back = function(keepItem)
 ScopeTree.prototype.goTo = function (id)
 {
     var self = this;
+
+    if (self._currentNode != self._initialNode) throw new Error("Cannot go to id '" + id + "' because current scope is not the initial.");
+
+    var toNode = self._nodes[id];
+    if (toNode === undefined) throw new Error("Node by id '" + id + "' not found.");
+    if (toNode === self._initialNode) throw new Error("Cannot go to id '" + id + "' because that is the initial.");
+    toNode.addAllFields(self.currentScope);
+    var parent = toNode.parent();
+    parent.forEachToRoot(function(node)
+    {
+        node.addNonExistingAndNonPrivateFields(self.currentScope);
+    });
+    self._currentNode = toNode;
+
+    this._refreshCurrentFields();
+}
+
+ScopeTree.prototype._refreshCurrentFields = function()
+{
+    this._currentFields = {};
+    for (var fn in this.currentScope)
+    {
+        this._currentFields[fn] = true;
+    }
 }
 
 module.exports = ScopeTree;
