310f136062d573a5d897cf5ba83efc4c6db0e5bf
gmezo <gabor.mezo@swicon.hu>
2014-08-06 17:19:56 +0200

    First bookmarking test passed

--- tests/index.js
+++ tests/index.js
@@ -1,2 +1,113 @@
-require("./activities");
-require("./hosting");
+//require("./activities");
+//require("./hosting");
+
+var Expression = require("../").activities.Expression;
+var Func = require("../").activities.Func;
+var Block = require("../").activities.Block;
+var activityMarkup = require("../").activities.activityMarkup;
+var ActivityExecutionEngine = require("../").activities.ActivityExecutionEngine;
+var _ = require("lodash");
+var ConsoleTracker = require("../").activities.ConsoleTracker;
+var WorkflowHost = require("../").hosting.WorkflowHost;
+var InstanceIdParser = require("../").hosting.InstanceIdParser;
+var assert = require("assert");
+
+describe("Bookmarking", function()
+{
+    it("should handle parallel activities", function (done)
+    {
+        var activity = activityMarkup.parse(
+            {
+                parallel: {
+                    var1: "",
+                    displayName: "Root",
+                    args: [
+                        {
+                            block: {
+                                displayName: "Wait Block 1",
+                                args: [
+                                    {
+                                        waitForBookmark: {
+                                            displayName: "Wait 1",
+                                            bookmarkName: "bm1"
+                                        }
+                                    },
+                                    {
+                                        func: {
+                                            displayName: "Func 1",
+                                            code: function ()
+                                            {
+                                                return this.var1 += "a";
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            block: {
+                                displayName: "Wait Block 2",
+                                args: [
+                                    {
+                                        waitForBookmark: {
+                                            displayName: "Wait 2",
+                                            bookmarkName: "bm2"
+                                        }
+                                    },
+                                    {
+                                        func: {
+                                            displayName: "Func 2",
+                                            code: function ()
+                                            {
+                                                return this.var1 += "b";
+                                            }
+                                        }
+                                    }
+                                ]
+                            }
+                        },
+                        {
+                            block: {
+                                displayName: "Resume Block",
+                                args: [
+                                    {
+                                        resumeBookmark: {
+                                            displayName: "Resume 1",
+                                            bookmarkName: "bm1"
+                                        }
+                                    },
+                                    {
+                                        resumeBookmark: {
+                                            displayName: "Resume 2",
+                                            bookmarkName: "bm2"
+                                        }
+                                    },
+                                    "bubu"
+                                ]
+                            }
+                        }
+                    ]
+                }
+            });
+
+        var engine = new ActivityExecutionEngine(activity);
+        engine.addTracker(new ConsoleTracker());
+
+        engine.invoke().then(
+            function (result)
+            {
+                try
+                {
+                    assert.ok(_.isArray(result));
+                    assert.equal(result.length, 3);
+                    assert.equal(result[0], "a");
+                    assert.equal(result[1], "ab");
+                    assert.equal(result[2], "bubu");
+                }
+                catch (e)
+                {
+                    assert.ifError(e);
+                }
+            }).nodeify(done);
+    });
+});
