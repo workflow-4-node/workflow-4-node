702fdec17896b343e087a90d7c1883185d2e87a3
gmezo <gabor.mezo@swicon.hu>
2014-08-14 14:45:27 +0200

    node.js 0.10.30 supported with traceur + node-proxy, I need to check the performance

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-module.exports = {
-    common: require("./lib/common/index"),
-    activities: require("./lib/activities/index"),
-    hosting: require("./lib/hosting/index")
+var Module = require('module');
+var originalRequireJs = Module._extensions['.js'];
+var traceur = require('traceur');
+
+try
+{
+    traceur.require.makeDefault(function (filename)
+    {
+        return /^(?=.*workflow-4-node[/\\]lib)+.+\.js$/.test(filename);
+    });
+
+    module.exports = {
+        common: require("./lib/common/index"),
+        activities: require("./lib/activities/index"),
+        hosting: require("./lib/hosting/index")
+    }
+}
+finally
+{
+    Module._extensions['.js'] = originalRequireJs;
 }
