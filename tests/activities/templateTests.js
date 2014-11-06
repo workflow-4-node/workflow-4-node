efd32b15e826a5f3eff22ee05b9f2807c4d48ac2
unbornchikken <gabor.mezo@outlook.com>
2014-11-06 16:45:33 +0100

    template support implemented

--- tests/activities/templateTests.js
+++ tests/activities/templateTests.js
@@ -4,7 +4,7 @@ var _ = require('lodash');
 var assert = require('assert');
 
 describe('Template', function() {
-    it('should parse object correctlt', function(done) {
+    it('should parse object correctly', function(done) {
 
         var engine = new ActivityExecutionEngine({
             template: {
@@ -29,6 +29,42 @@ describe('Template', function() {
         
         engine.invoke().then(function(result) {
             
+            assert.ok(_.isPlainObject(result));
+            assert.equal(result.a, 'foo');
+            assert.ok(_.isArray(result.b));
+            assert.equal(result.b.length, 3);
+            assert.equal(result.b[0], 'zoo');
+            assert.ok(_.isPlainObject(result.b[1]));
+            assert.equal(result.b[1].c, 6);
+            assert.equal(result.b[2], 42);
+        }).nodeify(done);
+    });
+
+    it('should work when specialized', function(done) {
+
+        var engine = new ActivityExecutionEngine({
+            block: [
+                {
+                    a: 'foo',
+                    b: [
+                        'zoo',
+                        {
+                            c: {
+                                '@func': {
+                                    code: function() {
+                                        return 6;
+                                    }
+                                }
+                            }
+                        },
+                        '# 42'
+                    ]
+                }
+            ]
+        });
+
+        engine.invoke().then(function(result) {
+
             assert.ok(_.isPlainObject(result));
             assert.equal(result.a, 'foo');
             assert.ok(_.isArray(result.b));
