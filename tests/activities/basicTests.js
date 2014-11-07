c16fcfac52fc4959346fd7fb899377407acfdba7
unbornchikken <gabor.mezo@outlook.com>
2014-11-07 15:31:50 +0100

    logic activities

--- tests/activities/basicTests.js
+++ tests/activities/basicTests.js
@@ -520,7 +520,9 @@ describe("If", function () {
                         if: {
                             condition: {
                                 func: {
-                                    code: function() { return false; }
+                                    code: function () {
+                                        return false;
+                                    }
                                 }
                             },
                             thenBody: {
@@ -551,5 +553,268 @@ describe("If", function () {
             function (result) {
                 assert.equal(2 + 5, result);
             }).nodeify(done);
+    });
+});
+
+describe('Logic Operators', function () {
+    describe('Truthy', function () {
+        it('should work', function (done) {
+            var engine = new ActivityExecutionEngine({
+                block: {
+                    t1: {
+                        truthy: {
+                            value: 'a'
+                        }
+                    },
+                    t2: {
+                        truthy: {
+                            value: null
+                        }
+                    },
+                    t3: {
+                        truthy: {
+                            value: true,
+                            is: 'is',
+                            isNot: 'isNot'
+                        }
+                    },
+                    t4: {
+                        truthy: {
+                            value: null,
+                            is: 'is',
+                            isNot: {
+                                func: {
+                                    code: function () {
+                                        return 'isNot';
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    args: [
+                        ['# this.get("t1")', '# this.get("t2")', '# this.get("t3")', '# this.get("t4")']
+                    ]
+                }
+            });
+
+            engine.invoke().then(
+                function (result) {
+                    assert.ok(_.isArray(result));
+                    assert.equal(result[0], true);
+                    assert.equal(result[1], false);
+                    assert.equal(result[2], 'is');
+                    assert.equal(result[3], 'isNot');
+                }).nodeify(done);
+        });
+    });
+
+    describe('Falsy', function () {
+        it('should work', function (done) {
+            var engine = new ActivityExecutionEngine({
+                block: {
+                    t1: {
+                        falsy: {
+                            value: 'a'
+                        }
+                    },
+                    t2: {
+                        falsy: {
+                            value: null
+                        }
+                    },
+                    t3: {
+                        falsy: {
+                            value: true,
+                            is: 'is',
+                            isNot: 'isNot'
+                        }
+                    },
+                    t4: {
+                        falsy: {
+                            value: null,
+                            is: '# "is"',
+                            isNot: {
+                                func: {
+                                    code: function () {
+                                        return 'isNot';
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    args: [
+                        ['# this.get("t1")', '# this.get("t2")', '# this.get("t3")', '# this.get("t4")']
+                    ]
+                }
+            });
+
+            engine.invoke().then(
+                function (result) {
+                    assert.ok(_.isArray(result));
+                    assert.equal(result[0], false);
+                    assert.equal(result[1], true);
+                    assert.equal(result[2], 'isNot');
+                    assert.equal(result[3], 'is');
+                }).nodeify(done);
+        });
+    });
+
+    describe('Equals', function () {
+        it('should work', function (done) {
+            var engine = new ActivityExecutionEngine({
+                block: {
+                    a: {
+                        equals: {
+                            value: function () {
+                                return 42;
+                            },
+                            to: '# 40 + 2 ',
+                            is: function () {
+                                return '42';
+                            },
+                            isNot: 'aba'
+                        }
+                    },
+                    b: {
+                        equals: {
+                            value: function () {
+                                return 42;
+                            },
+                            to: '# 40 + 1 ',
+                            is: function () {
+                                return '42';
+                            },
+                            isNot: 'aba'
+                        }
+                    },
+                    args: {
+                        a: '# this.get("a")',
+                        b: '# this.get("b")'
+                    }
+                }
+            });
+
+            engine.invoke().then(
+                function (result) {
+                    assert.ok(_.isPlainObject(result));
+                    assert.equal(result.a, '42');
+                    assert.equal(result.b, 'aba');
+                }).nodeify(done);
+        });
+    });
+
+    describe('NotEquals', function () {
+        it('should work', function (done) {
+            var engine = new ActivityExecutionEngine({
+                block: {
+                    a: {
+                        notEquals: {
+                            value: function () {
+                                return 42;
+                            },
+                            to: '# 40 + 2 ',
+                            is: function () {
+                                return '42';
+                            },
+                            isNot: 'aba'
+                        }
+                    },
+                    b: {
+                        notEquals: {
+                            value: function () {
+                                return 42;
+                            },
+                            to: '# 40 + 1 ',
+                            is: function () {
+                                return '42';
+                            },
+                            isNot: 'aba'
+                        }
+                    },
+                    args: {
+                        a: '# this.get("a")',
+                        b: '# this.get("b")'
+                    }
+                }
+            });
+
+            engine.invoke().then(
+                function (result) {
+                    assert.ok(_.isPlainObject(result));
+                    assert.equal(result.a, 'aba');
+                    assert.equal(result.b, '42');
+                }).nodeify(done);
+        });
+    });
+
+    describe('Not, And, Or', function () {
+        it('should work', function (done) {
+            var engine = new ActivityExecutionEngine({
+                block: {
+                    a: {
+                        and: [
+                            true,
+                            'bubu',
+                            {
+                                or: [
+                                    '# true',
+                                    false
+                                ]
+                            },
+                            {
+                                not: [
+                                    {
+                                        and: [
+                                            true,
+                                            function () {
+                                                return null;
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                    b: {
+                        and: {
+                            args: [
+                                {
+                                    or: [
+                                        '# true',
+                                        false
+                                    ]
+                                },
+                                {
+                                    not: [
+                                        {
+                                            and: [
+                                                true,
+                                                '# [ 42 ]'
+                                            ]
+                                        }
+                                    ]
+                                }
+                            ],
+                            isFalse: function () {
+                                return Promise.delay(100).then(function () {
+                                    return 42;
+                                });
+                            }
+                        }
+                    },
+                    args: {
+                        a: '# this.get("a")',
+                        b: '# this.get("b")'
+                    }
+                }
+            });
+
+            engine.invoke().then(
+                function (result) {
+                    assert.ok(_.isPlainObject(result));
+                    assert.equal(result.a, true);
+                    assert.equal(result.b, 42);
+                }).nodeify(done);
+        });
     });
 });
